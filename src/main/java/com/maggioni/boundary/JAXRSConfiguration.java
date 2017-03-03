6ed0671dd71755b2ebccc6d696b0e836d8a1915e
magang <magang@magang-PC.fritz.box>
2017-03-03 11:22:41 +0100

    first draft

---
+++ src/main/java/com/maggioni/boundary/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.maggioni.boundary;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("res")
+public class JAXRSConfiguration  extends Application{
+    
+}
